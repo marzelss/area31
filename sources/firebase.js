239964efe41eae61d91e32595c6db8827c0a24d4
Martina <martinacaccamo95@gmail.com>
2026-03-13 23:45:12 +0100

    Update firebase.js

--- sources/firebase.js
+++ sources/firebase.js
@@ -1,19 +1,17 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-storage.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB_I_7YGhqBAAeEMpJI2ZiZ9tnCfa9myEw",
-    authDomain: "area31-922e5.firebaseapp.com",
-    projectId: "area31-922e5",
-    storageBucket: "area31-922e5.firebasestorage.app",
-    messagingSenderId: "330214881157",
-    appId: "1:330214881157:web:8492f1995cc5ba25d9f6ec"
+  apiKey: "AIzaSyB_I_7YGhqBAAeEMpJI2ZiZ9tnCfa9myEw",
+  authDomain: "area31-922e5.firebaseapp.com",
+  projectId: "area31-922e5",
+  storageBucket: "area31-922e5.firebasestorage.app",
+  messagingSenderId: "330214881157",
+  appId: "1:330214881157:web:8492f1995cc5ba25d9f6ec",
+  databaseURL: "https://area31-922e5-default-rtdb.firebaseio.com"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export storage so other scripts can use it
-export const storage = getStorage(app);
+export const db = getDatabase(app);
