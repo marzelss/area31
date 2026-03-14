880635bdcd8817c73e9bc8e7671dc4e06ecc528a
Martina <martinacaccamo95@gmail.com>
2026-03-14 22:01:36 +0100

    fixed emojis

--- protocol/protocol.js
+++ protocol/protocol.js
@@ -143,16 +143,16 @@ async function init() {
         { text: strings.meeting },
         { text: "" },
 
-        { text: `🗓️ ${strings.date}` },
-        { text: `🕤 ${strings.time}` },
-        { text: `📍 ${strings.location}` },
+        { text: `>> ${strings.date}` },
+        { text: `>> ${strings.time}` },
+        { text: `>> ${strings.location}` },
         { text: "" },
 
         { text: strings.taskIntro1 },
         { text: strings.taskIntro2 },
         { text: "" },
 
-        { text: `📎 ${strings.task}: ${roleTask}` },
+        { text: `➡️ ${strings.task}: ${roleTask}` },
         { text: `❇️ ${strings.bonus}` },
         { text: "" },
 
